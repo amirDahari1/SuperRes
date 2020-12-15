0220084e8e2ff425f5a8fbcf46ce6a83c79f11bb
amir.dahari <amir.dahari@mail.huji.ac.il>
2020-12-15 16:30:58 +0000

    working directory

--- code/Architecture.py
+++ code/Architecture.py
@@ -30,8 +30,8 @@ nc_g = 2  # two phases for the generator input
 nc_d = 3  # three phases for the discriminator input
 
 # Width generator channel hyperparameter
-wd = 7
-wg = 7
+wd = 8
+wg = 9
 
 # Number of training epochs
 num_epochs = 5
