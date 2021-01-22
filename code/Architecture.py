89ba5987593f1bdc8780e8715547162513973cf5
amir.dahari <amir.dahari@mail.huji.ac.il>
2021-01-22 18:07:26 +0000

    G width 8 D width 7

--- code/Architecture.py
+++ code/Architecture.py
@@ -35,8 +35,8 @@ nc_g = 2  # two phases for the generator input
 nc_d = 3  # three phases for the discriminator input
 
 # Width generator channel hyperparameter
-wd = 8
-wg = 10
+wd = 7
+wg = 8
 
 # Number of training epochs
 num_epochs = 400
