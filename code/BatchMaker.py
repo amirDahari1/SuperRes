cef10760a9c1c864f26317ab271e497b4008404f
amir.dahari <amir.dahari@mail.huji.ac.il>
2021-02-09 15:52:56 +0000

    trying to branch out of main

--- code/BatchMaker.py
+++ code/BatchMaker.py
@@ -6,10 +6,10 @@ import torch
 import torch.utils.data
 import ImageTools
 
-
-LOW_RES = 32
-HIGH_RES = 128
+LOW_RES = 32  # the low resolution number of pixels LOW_RESxLOW_RES
+HIGH_RES = 128  # the high resolution number of pixels HIGH_RESxHIGH_RES
 N_SAMPLES = 10000
+CROP = 4  # crop pixels in each dimension when choosing train slices
 
 if os.getcwd().endswith('code'):
     os.chdir('..')  # current directory from /SuperRes/code to SuperRes/
@@ -64,11 +64,12 @@ class BatchMaker:
         :return: A random image of size res from the dimension chosen of the
         image.
         """
-        slice_chosen = random.randint(0, self.min_d - 1)  # the slice chosen
+        slice_chosen = random.randint(CROP, self.min_d - 1 - CROP)  # the
+        # slice chosen
         lim_pix = self.min_d - self.high_res  # the maximum pixel to start with
         # the starting pixels of the other dimensions:
-        pix1 = random.randint(0, lim_pix)
-        pix2 = random.randint(0, lim_pix)
+        pix1 = random.randint(CROP, lim_pix - CROP)
+        pix2 = random.randint(CROP, lim_pix - CROP)
         if dim_chosen == 0:
             res_image = self.im_3d[slice_chosen, pix1:pix1 + self.high_res,
                                    pix2:pix2 + self.high_res]
