b4bbe4b5a8e5d4b1622506da2408f0209e6b18f0
amirDahari1 <amir.dahari@mail.huji.ac.il>
2021-05-12 16:27:56 +0100

    evaluation on server

--- code/Evaluation.py
+++ code/Evaluation.py
@@ -16,11 +16,11 @@ progress_dir, wd, wg = args.directory, args.widthD, args.widthG
 n_res_blocks, pix_distance = args.n_res_blocks, args.pixel_coefficient_distance
 num_epochs, g_update, n_dims = args.num_epochs, args.g_update, args.n_dims
 squash, phases_to_low = args.squash_phases, args.phases_low_res_idx
-D_dimensions_to_check = args.d_dimensions_to_check
+D_dimensions_to_check, scale_f = args.d_dimensions_to_check, args.scale_factor
 
 progress_main_dir = 'progress/' + progress_dir
 path_to_g_weights = progress_main_dir + '/g_weights.pth'
-G_image_path = 'data/lower_res_separator_3d.tif'
+G_image_path = 'data/test_cube_sofc.tif'
 file_name = 'generated_tif.tif'
 crop_to_cube = True
 
@@ -43,10 +43,10 @@ if squash:
 else:
     nc_g = 1 + to_low_idx.size()[0]  # channel for pore plus number of
     # material phases to low res.
-nc_d = 2  # three phases for the discriminator input
+nc_d = 3  # three phases for the discriminator input
 
-BM = BatchMaker.BatchMaker(path=G_image_path, device=device)
-G_net = Networks.generator(ngpu, wg, nc_g, nc_d, n_res_blocks, n_dims).to(device)
+BM = BatchMaker.BatchMaker(path=G_image_path, device=device, sf=scale_f, dims=n_dims, crop=False)
+G_net = Networks.generator(ngpu, wg, nc_g, nc_d, n_res_blocks, n_dims, BM.scale_factor).to(device)
 G_net.load_state_dict(torch.load(path_to_g_weights, map_location=torch.device(
     device)))
 G_net.eval()
@@ -57,14 +57,15 @@ def save_tif_3d(network_g, high_res_im, grey_idx, device, filename,
     """
         Saves a tif image of the output of G on all of the 3d image high_res_im
     """
-    scale_factor = BM.low_l / BM.high_l
+    print(grey_idx)
+    print(high_res_im.size())
     low_res_input = LearnTools.down_sample_for_g_input(high_res_im,
                                                        grey_idx,
-                                                       scale_factor,
+                                                       BM.scale_factor,
                                                        device, n_dims)
     print(low_res_input.size())
 
-    g_output = network_g(low_res_input, mask).detach().cpu()
+    g_output = network_g(low_res_input).detach().cpu()
     g_output = ImageTools.fractions_to_ohe(g_output)
     g_output_grey = ImageTools.one_hot_decoding(g_output).astype('uint8')
     imsave(progress_main_dir + '/' + filename, g_output_grey)
@@ -80,7 +81,8 @@ def save_tif_3d(network_g, high_res_im, grey_idx, device, filename,
 with torch.no_grad():  # save the images
     im_3d = BM.all_image_batch()
     if crop_to_cube:
-        min_d = min(im_3d.size()[2:])//3
+        min_d = 128
         im_3d = im_3d[:, :, :min_d, :min_d, :min_d]
     save_tif_3d(G_net, im_3d, to_low_idx, device, file_name,
                 mask=False)
+    
