7cf604498bd44a240f6e35f2f3d8d0c6f950afc4
amirDahari1 <amir.dahari@mail.huji.ac.il>
2021-05-27 11:53:11 +0100

    Evaluation.py updated for stitching together small cubes for a large volume

--- code/Evaluation.py
+++ code/Evaluation.py
@@ -15,20 +15,20 @@ args = LearnTools.return_args(parser)
 progress_dir, wd, wg = args.directory, args.widthD, args.widthG
 n_res_blocks, pix_distance = args.n_res_blocks, args.pixel_coefficient_distance
 num_epochs, g_update, n_dims = args.num_epochs, args.g_update, args.n_dims
-squash, phases_to_low = args.squash_phases, args.phases_low_res_idx
+squash = args.squash_phases
 D_dimensions_to_check, scale_f = args.d_dimensions_to_check, args.scale_factor
 
+phases_to_low = 1
+down_sample = False
 
-# progress_main_dir = 'progress/' + progress_dir
-progress_main_dir = 'progress'
-# path_to_g_weights = progress_main_dir + '/g_weights.pth'
-path_to_g_weights = progress_main_dir + '/g_weights_64_more_slices_3k.pth'
+progress_main_dir = 'progress/' + progress_dir
+# progress_main_dir = 'progress'
+path_to_g_weights = progress_main_dir + '/g_weights.pth'
+# path_to_g_weights = progress_main_dir + '/g_weights_large_slice.pth'
 # G_image_path = 'data/separator_wo_fibrils.tif'
-G_image_path = 'data/nmc_crop.tif'
-# D_image_path = 'data/separator_all_grey.tif'
-D_image_path = 'data/nmc_crop.tif'
+G_image_path = 'data/downsample_vol_train_nmc.tif'
 
-file_name = 'generated_tif2.tif'
+file_name = 'generated_tif.tif'
 crop_to_cube = False
 
 # TODO all of these (ngpu, device, to_low_idx, nc_g..) can go into a
@@ -54,9 +54,7 @@ nc_d = 3  # three phases for the discriminator input
 
 
 BM_G = BatchMaker.BatchMaker(path=G_image_path, device=device,
-                            rot_and_mir=False)
-BM_D = BatchMaker.BatchMaker(path=D_image_path, device=device,
-                            rot_and_mir=False)
+                            rot_and_mir=False, low_res=not down_sample)
 G_net = Networks.generator(ngpu, wg, nc_g, nc_d, n_res_blocks, n_dims,
                            scale_factor=scale_f).to(device)
 G_net.load_state_dict(torch.load(path_to_g_weights, map_location=torch.device(
@@ -64,39 +62,93 @@ G_net.load_state_dict(torch.load(path_to_g_weights, map_location=torch.device(
 G_net.eval()
 
 
-def save_tif_3d(network_g, high_res_im, original_im, grey_idx, device,
-                filename,
-                mask=False):
+def save_tif_3d(network_g, input_to_g, grey_idx, device,
+                filename):
     """
         Saves a tif image of the output of G on all of the 3d image high_res_im
     """
-    print(high_res_im.size())
-    low_res_input = LearnTools.down_sample_for_g_input(high_res_im,
+    print(input_to_g.size())
+    if down_sample:
+        input_to_g = LearnTools.down_sample_for_g_input(input_to_g,
                                                        grey_idx,
                                                        scale_f,
                                                        device, n_dims)
-    print(low_res_input.size())
+    print(input_to_g.size())
 
-    g_output = network_g(low_res_input).detach().cpu()
+    g_output = network_g(input_to_g).detach().cpu()
     g_output = ImageTools.fractions_to_ohe(g_output)
     g_output_grey = ImageTools.one_hot_decoding(g_output).astype('uint8')
     imsave(progress_main_dir + '/' + filename, g_output_grey)
-    low_res_grey = ImageTools.one_hot_decoding(low_res_input.cpu()).astype(
+    low_res_grey = ImageTools.one_hot_decoding(input_to_g.cpu()).astype(
         'uint8')
     imsave(progress_main_dir + '/low_res' + filename,
            low_res_grey)
-    high_res_im = ImageTools.one_hot_decoding(original_im.cpu()).astype('uint8')
-    imsave(progress_main_dir + '/' + filename + '-original',
-           high_res_im)
+    # high_res_im = ImageTools.one_hot_decoding(original_im.cpu()).astype('uint8')
+    # imsave(progress_main_dir + '/' + filename + '-original',
+    #        high_res_im)
 
 
 with torch.no_grad():  # save the images
     im_3d = BM_G.all_image_batch()
-    orig_im_3d = BM_D.all_image_batch()
-    if crop_to_cube:
-        min_d = 128
-        im_3d = im_3d[:, :, :min_d, :min_d, :min_d]
-        orig_im_3d = orig_im_3d[:, :, :min_d, :min_d, :min_d]
-    save_tif_3d(G_net, im_3d, orig_im_3d, to_low_idx, device, file_name,
-                mask=False)
+    # orig_im_3d = BM_D.all_image_batch()
+    # if crop_to_cube:
+    #     min_d = 128
+    #     im_3d = im_3d[:, :, :min_d, :min_d, :min_d]
+        # orig_im_3d = orig_im_3d[:, :, :min_d, :min_d, :min_d]
+    # save_tif_3d(G_net, im_3d, to_low_idx, device, file_name)
     
+    nz1, nz2, nz3 = 256, 256, 256
+    im_3d = im_3d[..., :nz1, :nz2, :nz3].to(device)
+    step_len = 32
+    overlap = 16
+    high_overlap = int(overlap/2 * scale_f)
+    step = step_len - overlap
+    first_img_stack = []
+    with torch.no_grad():
+        last_ind1 = int((nz1-step_len)//step)
+        for i in range(last_ind1 + 1):
+            print('i = ' + str(i))
+            first_lr_vec = im_3d[..., i*step:i*step+step_len, :, :]
+            second_img_stack = []
+            last_ind2 = int((nz2-step_len)//step)
+            for j in range(last_ind2 + 1):
+                print(j)
+                second_lr_vec = first_lr_vec[..., :, j * step:j * step +
+                                                             step_len, :]
+                third_img_stack = []
+                last_ind3 = int((nz3 - step_len) // step)
+                for k in range(last_ind3 + 1):
+                    print(k)
+                    third_lr_vec = second_lr_vec[..., :,
+                                                 :, k * step:k * step + step_len]
+                    g_output = G_net(third_lr_vec).detach().cpu()
+                    g_output = ImageTools.fractions_to_ohe(g_output)
+                    g_output_grey = ImageTools.one_hot_decoding(
+                        g_output).astype('uint8').squeeze()
+                    if k == 0:
+                        g_output_grey = g_output_grey[:, :, :-high_overlap]
+                    elif k == last_ind3:
+                        g_output_grey = g_output_grey[:, :, high_overlap:]
+                    else:
+                        g_output_grey = g_output_grey[:, :, high_overlap:
+                                                      - high_overlap]
+                    third_img_stack.append(np.uint8(g_output_grey))
+                res2 = np.concatenate(third_img_stack, axis=2)
+                if j == 0:
+                    res2 = res2[:, :-high_overlap, :]
+                elif j == last_ind2:
+                    res2 = res2[:, high_overlap:, :]
+                else:
+                    res2 = res2[:, high_overlap:-high_overlap, :]
+                second_img_stack.append(res2)
+            res1 = np.concatenate(second_img_stack, axis=1)
+            if i == 0:
+                res1 = res1[:-high_overlap, :, :]
+            elif i == last_ind1:
+                res1 = res1[high_overlap:, :, :]
+            else:
+                res1 = res1[high_overlap:-high_overlap, :, :]
+            first_img_stack.append(res1)
+    img = np.concatenate(first_img_stack, axis=0)
+    imsave(progress_main_dir + '/' + file_name, img)
+    # np.save('large_vol_g_2_nmc', img)
